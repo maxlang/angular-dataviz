6a0edf02b692413a989713c5b273cf3687074341
Max Lang <max@blendlabsinc.com>
2013-03-06 00:53:07 -0800

    changed data model and added a display of the filtered data

--- doc/js/demoApp.js
+++ doc/js/demoApp.js
@@ -18,7 +18,7 @@ angular.module('demoApp', ['dataviz'], function($locationProvider) {
       $('body,html').animate({ scrollTop: top }, 800);
     });
   };
-}]).filter('rangeToDay', [function() {
+}]).filter('timestampToDay', [function() {
       return function(input) {
         console.log('filter');
         console.log(input);
@@ -27,21 +27,47 @@ angular.module('demoApp', ['dataviz'], function($locationProvider) {
         }
         var output =  input.map(function(e) {
           var d = new Date();
-          d.setTime(e.start);
+          d.setTime(e.time);
           return {
             date: d.getFullYear() + "-" + (d.getMonth()+1) + "-" + d.getDate(),
-            count: e.count
+            count: e.bites
           };
         });
         console.log(output);
         return output;
       };
     }])
+    .filter('inView', [function() {
+      return function(records, queryObject){
+        var i;
+        var output = [];
+        for(i=0;i<records.length;i++) {
+          var record = records[i];
+          if ((!queryObject.from || record.time >= queryObject.from) && (!queryObject.to || record.time < queryObject.to)) {
+            console.log(queryObject);
+            output.push(record);
+          }
+        }
+        return output;
+      }
+    }])
+    .directive('calendarConverter', [function() {
+      return {
+        restrict:'E',
+        scope: {
+          data: '=',
+          query: '='
+        },
+    controller: 'queryConverterCtrl'
+      }
+    }])
     .controller('queryConverterCtrl',['$scope','$rootScope','$filter', function($scope, $rootScope, $filter) {
       console.log('controller');
       console.log($scope);
       $scope.dataObject = $rootScope.dataObject;
-      $scope.counts = $filter('rangeToDay')($scope.dataObject.dateRangeCounts);
+      $scope.counts = $filter('timestampToDay')($scope.dataObject.records);  //TODO TDOD:sum
+
+
       $scope.queryObject = $rootScope.queryObject;
       $scope.selectedRanges = [{start:null,end:null}];
       console.log($scope);
@@ -84,28 +110,36 @@ angular.module('demoApp', ['dataviz'], function($locationProvider) {
       from: null,
       to: null
     };
+
+    var carnivores = ["Rex", "Allen", "Velossy"];
+    var herbivores = ["Steggy", "Trice", "Bronta"];
+
+
+
     var populate = function(dataObj) {
-      console.log("populating");
+      console.log("populating records");
         var today = new Date();
         var dayInMilliseconds = 24*60*60*1000;
         var end = today.getTime();
-        var start = end - 100 * dayInMilliseconds;
-        dataObj.dateRangeCounts = [];
-        dataObj.dateDayCounts = [];
+        var start = end - 200 * dayInMilliseconds;
+        dataObj.records = [];
+        //TODO: irrelevant, but how do you make a good random time series?
         for( ;start<=end;start+=dayInMilliseconds) {
-          dataObj.dateRangeCounts.push({
-            start: start,
-            end: start+dayInMilliseconds,
-            count: Math.floor(Math.pow(Math.random(), 3) * 10)
-          });
-          var day = new Date();
-          day.setTime(start);
+          var count = Math.floor(Math.pow(Math.random(), 3) * 10);
+          var i;
+          for (i=0;i<count;i++) {
+            var car = Math.floor(Math.random() * 3);
+            var veg = Math.floor(Math.random() * 3);
+            var bites = 1 + Math.floor(Math.random() * 3);
+            var time = start + Math.floor(Math.random() * dayInMilliseconds);
+            dataObj.records.push({
+              eater: carnivores[car],
+              eaten: herbivores[veg],
+              bites: bites,
+              time: time
+            })
 
-          dataObj.dateDayCounts.push({
-            date: day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate(),
-            count: Math.floor(Math.pow(Math.random(), 3) * 10)
-          });
-
+          }
         }
       console.log(dataObj);
     }
@@ -114,5 +148,13 @@ angular.module('demoApp', ['dataviz'], function($locationProvider) {
     populate($scope.dataObject);
       console.log('dataObject');
     console.log($scope.dataObject);
-}]);
+}]).filter('timestampToTime',function() {
+      return function(t) {
+        var d = new Date();
+        d.setTime(t);
+        return d.toDateString();
+      }
+
+    });
+
 
