e2f3986722b0e2c2e487d89520b4fbe68b834611
Max Lang <max@blendlabsinc.com>
2014-04-09 17:07:50 -0700

    multi line chart with legend

--- modules/directives/animated/aLinechart.js
+++ modules/directives/animated/aLinechart.js
@@ -10,7 +10,7 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
       //note: currently the line is meant to be styled with css, for example:
       //stroke-width: 2px;
       //stroke-opacity: 1;
-     // stroke: black;
+      // stroke: black;
 
 
 
@@ -29,7 +29,8 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
         'realtime' : true,
         'snap' : false,
         'filterSelector' : false,
-        'date': false
+        'date': false,
+        'multi':false
       });
       var initialized = false;
 
@@ -51,7 +52,6 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
           Array.prototype.splice.apply(scope.data,[0,scope.data.length].concat(ensureDates));
         }
 
-
         if(!initialized) {
           init(data);
         }
@@ -63,7 +63,7 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
         change();
       }, true);
 
-      var width, height, margins, range, max, leftMargin, w, h, bars, barPadding, barWidth, svg, g, x, y, d, xAxis, yAxis, xAxisG, yAxisG, line, path, brush, brushRect;
+      var width, height, margins, range, max, leftMargin, rightMargin, w, h, bars, barPadding, barWidth, svg, g, x, y, d, xAxis, yAxis, xAxisG, yAxisG, line, path, paths, brush, brushRect, labels, keys, legendDims, legend;
 
       var calcInfo = function(data) {
         width = o('widthPx');
@@ -136,7 +136,7 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
 
         if(range === 'auto') {
           var yMax;
-            yMax = max;
+          yMax = max;
           //var yMin = data[data.length - 1].value;
           y = d3.scale.linear().domain([0, yMax]).range([h, 0]);
         } else {
@@ -159,10 +159,146 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
 
       };
 
+      var calcInfoMulti = function(data) {
+        width = o('widthPx');
+        height = o('heightPx');
+        margins = o('margins');
+        range = o('range');
+
+        var flattenedData = _.flatten(_.pluck(data, 'data'));
+        labels = _.pluck(data, 'key');
+        legendDims = calcLegendDims(labels);
+
+        h = height - margins.top - margins.bottom;
+
+        var yTicks = h/60;
+
+        /**
+         *  Max value just used to judge height of y axis
+         *  1) defined explicitly
+         *  2) highest elt in range
+         *  3) highest elt in data
+         */
+        if (range !== 'auto') {
+          max = o('max') || (range && _.isArray(range) && range[1]) || _.max(_.pluck(flattenedData, 'value'));
+        } else {
+          max = _.max(_.pluck(flattenedData, 'value'));
+        }
+
+        leftMargin = margins.left;
+        rightMargin = margins.right;
+
+        if (o('autoMargin')) {
+          //account for possible decimals
+          var decimals = Math.ceil(-((Math.log(max/yTicks)/Math.log(10))));
+          if (decimals > 0) {
+            max = Math.floor(max);
+            max += "." + Math.pow(10, decimals);
+          }
+          var maxString = max;
+          //add commas
+          if (_.isNumber(max) && ((max + 1)!==max)) {
+            maxString += (Array(Math.floor((max+"").length/3)).join(","));
+          }
+
+          leftMargin = (margins.left + VizUtils.measure(maxString, element[0], "y axis").width) || margins.left;
+          leftMargin = leftMargin === -Infinity ? 0 : leftMargin;
+          leftMargin += 9;
+
+
+          rightMargin = rightMargin || 0;
+          rightMargin += legendDims.width;
+        }
+
+        w = width - leftMargin - rightMargin;
+        if (range !== 'auto') {
+          bars = o('bars') || flattenedData.length;
+        } else {
+          var interval = o('interval') || 1; //TODO: handle case where interval is undefined - perhaps extrapolate from data somehow?
+          var keys = _.pluck(flattenedData, 'key');
+          var maxKey = _.max(keys);
+          var minKey = _.min(keys);
+          bars = Math.ceil((maxKey - minKey)/interval) + 1;
+        }
+        barPadding = o('padding');
+
+        barWidth = (w/bars) - barPadding;
+
+        d = o('domain');
+
+        if(d === 'auto') {
+
+          var xMax = _.max(_.pluck(flattenedData, 'key'));
+          var xMin = _.min(_.pluck(flattenedData, 'key'));
+          //TODO: use d3 to do this automatically
+          xMax += (xMax - xMin)/bars;
+          x = d3.scale.linear().domain([xMin, xMax]).range([0, w]);
+        } else {
+          x = d3.scale.linear().domain(d).range([0, w]);
+        }
+
+        if(range === 'auto') {
+          var yMax;
+          yMax = max;
+          //var yMin = data[data.length - 1].value;
+          y = d3.scale.linear().domain([0, yMax]).range([h, 0]);
+        } else {
+          y = d3.scale.linear().domain(range).range([h, 0]);
+        }
+
+
+        xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(w/100);
+        if (o('date')) {
+          xAxis.tickFormat(function(d) {
+            return moment(d).format('YYYY-MM-DD');
+          });
+        }
+
+        yAxis = d3.svg.axis().scale(y).orient("left").ticks(yTicks);
+
+        line = d3.svg.line()
+            .x(function(d, i) { return x(d.key); })
+            .y(function(d, i) { return y(d.value); });
+
+        legendDims.top = margins.top;
+        legendDims.left = leftMargin + w;
+
+
+      };
+
+      var legendSquareSizePx = 15;
+      var legendPadding = 10;
+      var legendPaddingLeft = 30;
+      var legendSpacing = 3;
+      var maxLegendWidth = 400;  //TODO: enforce
+      var minLegendWidth = 100;
+
+
+      function calcLegendDims(labels) {
+        var slices = labels;
+//        if (data.length >= o('maxSlices')) {
+//          _.last(slices).key = "Other";
+//        }
+
+        return {
+          width: Math.min(Math.max(( legendPadding + legendPaddingLeft) +
+                  legendSquareSizePx +
+                  legendSpacing + _.max(_(slices).map(function(v) {return VizUtils.measure(v, element[0], 'legend-text').width;}).value()),
+              minLegendWidth), maxLegendWidth),
+          height: (slices.length * legendSquareSizePx) +
+              (2 * legendPadding) +
+              ((slices.length - 1) * legendSpacing)
+        };
+      }
+
       function init(data) {
         $(element[0]).html('<svg></svg>');
 
-        calcInfo(data);
+        if (o('multi')) {
+          calcInfoMulti(data);
+        } else {
+          calcInfo(data);
+        }
 
         svg = d3.select(element[0]).select("svg")
             .attr("width", width)
@@ -178,13 +314,27 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
 
         scope.brush.x(x);
 
-        path = g.append("path")
-            .datum(scope.data)
-            .attr("class", "line")
-            .attr("fill", "none")
+
+        if (o('multi')) {
+          paths = g.selectAll("path");
+//              .data(scope.data, function(d) {return d && d.data;}).enter().append("path")
+//              .classed("line", "true")
+//              .attr("class", function(d, i) {
+//                return "line-" + i;
+//              })
+//              .attr("fill", "none")
+////            .attr("stroke", "black")
+////            .attr("stroke-width", 2)
+//              .attr("d", line);
+        } else {
+          path = g.append("path")
+              .datum(scope.data)
+              .attr("class", "line")
+              .attr("fill", "none")
 //            .attr("stroke", "black")
 //            .attr("stroke-width", 2)
-            .attr("d", line);
+              .attr("d", line);
+        }
 
         xAxisG =   svg.append("g")
             .attr("class", "x axis")
@@ -208,12 +358,25 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
             .attr("y", -6)
             .attr("height", h+8);
 
+
+        legend = svg.append('g')
+            .attr("class", "legend")
+            .attr("width", legendDims.width)
+            .attr("height", legendDims.height)
+            .attr("transform", "translate(" + legendDims.left + "," + legendDims.top + ")");
+
+        keys = legend.selectAll("g");
+
         initialized = true;
       }
 
       function change() {
-        calcInfo(scope.data);
-//
+        if (o('multi')) {
+          calcInfoMulti(scope.data);
+        } else {
+          calcInfo(scope.data);
+        }
+
         svg.transition().duration(300)
             .attr("width", width)
             .attr("height", height)
@@ -229,10 +392,30 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
             .x(function(d) { return x(d.key); })
             .y(function(d) { return y(d.value); });
 
+        if (o('multi')) {
+          paths = paths.data(scope.data);
+          paths.enter().append("path")
+              .attr("class", function(d, i) {return "line-" + i;})
+              .classed("line", true)
+              .attr("fill", "none")
+//            .attr("stroke", "black")
+//            .attr("stroke-width", 2)
+              .attr("d", function(d, i) {
+                return line(d.data);
+              });
+          paths
+              .transition()
+              .duration(300)
+              .ease("linear")
+              .attr("d", function(d, i) {
+                return line(d.data);
+              });
+        } else {
           path.datum(scope.data).transition()
-            .duration(300)
-            .ease("linear")
-            .attr("d", line);
+              .duration(300)
+              .ease("linear")
+              .attr("d", line);
+        }
 
 //        lines.exit().transition().duration(300).remove();
 //
@@ -285,6 +468,52 @@ angular.module('dataviz.directives').directive('aLinechart', ['$timeout', 'VizUt
             .selectAll('rect')
             .attr("height", h+8);
 
+
+        legend.transition().duration(300)
+            .attr("width", legendDims.width)
+            .attr("height", legendDims.height)
+            .attr("transform", "translate(" + legendDims.left + "," + legendDims.top + ")");
+
+        keys = keys.data(labels);
+
+        var k = keys.enter().append("g")
+            .attr("transform", function(d, i) { /*console.log(d,i);*/ return "translate(" + legendPaddingLeft + "," + (legendPadding + (i * (legendSpacing + legendSquareSizePx))) + ")"; });
+
+        k.append("rect")
+            .attr("width", legendSquareSizePx)
+            .attr("height", legendSquareSizePx)
+            .attr("class", function(d, i) { return "line-" + i; } );
+
+        var nonTextWidth = legendSquareSizePx + (legendPadding + legendPaddingLeft) + legendSpacing;
+        var textWidth = Math.min((legendDims.width - nonTextWidth), (width - nonTextWidth));
+
+        var tc = d3.rgb(o('textColor'));
+        var rgba = [tc.r, tc.g, tc.b, o('fillOpacity')];
+
+        var text = k.append("foreignObject")
+            .attr("x", legendSquareSizePx + legendPadding + legendSpacing)
+            .attr("y", 0)
+            .attr('width', textWidth)
+            .attr('height', '1.2em')
+            .append("xhtml:div")
+            .html(function(d, i) { return "<div style='width:" + textWidth + "px; height:1.2em; overflow:hidden; text-overflow:ellipsis;color: rgba(" + rgba.join(',') + ");white-space:nowrap'>" + d + "</div>";});
+//
+        keys.transition().duration(300)
+            .attr("transform", function(d, i) { /*console.log(d,i);*/ return "translate(" + legendPaddingLeft + "," + (legendPadding + (i * (legendSpacing + legendSquareSizePx))) + ")"; })
+            .call(function() {
+              //TODO: incorporate into transition better
+              $(this[0]).width(textWidth).find('foreignObject').attr('class','legend-text').attr('width', textWidth).find('div').width(textWidth).css('color', "rgba(" + rgba.join(',') + ")");
+            });
+
+
+//        k.append("text")
+//            .attr("x", o('legendSquareSizePx') + o('legendPadding') + o('legendSpacing'))
+//            .attr("y", 9)
+//            .attr("dy", ".35em")
+//            .text(function(d) {
+//              return d.data.key; }).classed('pie-legend', true);
+
+        keys.exit().remove();
 
 //        g.transition()
 //            .duration(300)
